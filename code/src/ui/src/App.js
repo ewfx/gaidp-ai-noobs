68c614efa89b55e137447ff0c1092b3068f4308b
Archit Badola <badola.archit20@hotmail.com>
2025-03-23 18:49:36 +0530

    ReactJS setup

---
+++ code/src/ui/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FileUploader from "./components/FileUploader";
+
+const App = () => {
+  return (
+    <div className="mt-5">
+      <FileUploader/>
+    </div>
+  );
+};
+
+export default App;
